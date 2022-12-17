6557b9c5d6a7d2802b8d8319358551c3d96afd44
Steve Hou <stevehoho@hotmail.com.tw>
2022-12-17 09:46:00 +0800

    modify

--- flask1/main.py
+++ flask1/main.py
@@ -1,7 +1,11 @@
-from flask import Flask
+from flask import Flask ,render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     return "<h1> hello world </h1>"
+
+@app.route("/bootstrap")
+def bootstrap():
+    return "<h1>Bootstrap</h1>"
